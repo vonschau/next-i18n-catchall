2b55fabc5dafda0b31d046bc607068e08a3abc9a
Martin Cajthaml <martin.cajthaml@symbio.agency>
2021-01-15 17:12:28 +0100

    Update invalidate.ts

--- pages/api/invalidate.ts
+++ pages/api/invalidate.ts
@@ -2,7 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    console.log(req.method);
+
     fs.readdir('./', (err, files) => {
+        console.log(err?.message);
         console.log(files);
     });
 
